600d39f9c4cf8e75f0a0dcddde22736a8f5b16bb
Marc Villacorta <marc.villacorta@gmail.com>
2012-10-21 00:50:18 +0200

    Move host data to hiera.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,8 @@
-class hosts {
+class hosts ( $hosts ){
 
     resources { 'host':
         purge => true,
     }
 
-    host {
-
-        'localhost':
-            ensure       => present,
-            ip           => '127.0.0.1',
-            host_aliases => 'localhost.localdomain';
-
-        'puppet':
-            ensure       => present,
-            ip           => extlookup('host/puppet'),
-            host_aliases => "puppet.${::domain}";
-    }
+    create_resources(host, $hosts)
 }
